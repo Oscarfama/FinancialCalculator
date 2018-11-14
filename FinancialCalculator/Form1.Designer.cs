8c60f4f7ccd43709c00bb482ae64cdabd1c53441
erickgtzh <0193662@up.edu.mx>
2018-11-14 16:55:04 -0600

    Title change

--- FinancialCalculator/Form1.Designer.cs
+++ FinancialCalculator/Form1.Designer.cs
@@ -32,11 +32,11 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Financial engineering";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
